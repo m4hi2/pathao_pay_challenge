a37a6109d0bc38b97afb62e9dff30bb1d811af40
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-10-30 07:43:08 +0600

    feat: new user model and schema

--- src/digital_wallet_api/models.py
+++ src/digital_wallet_api/models.py
@@ -1,5 +1,14 @@
 from datetime import datetime
-
-from sqlalchemy import Column, DateTime, Integer, String, Float
+from sqlalchemy import Column, DateTime, Integer, String
 
 from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255))
+    email = Column(String(255), index=True)
+    pin = Column(String)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
