1f027f3138727a3ad486e6c6cafc9127566b1396
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-10-30 17:19:19 +0600

    docs: wallet view

--- src/digital_wallet_api/views/wallet.py
+++ src/digital_wallet_api/views/wallet.py
@@ -5,6 +5,14 @@ from .utils import convert_paisa_to_taka
 
 
 def get_system_balance(db: Session):
+    """Gets the total balance of all wallets present in the system.
+
+    Args:
+        db (Session): database connection
+
+    Returns:
+        balance (schemas.SystemBalance): total balance in the system
+    """
     system_balance = repository.wallet.get_all_wallet_balance_sum(db)
     system_balance_in_taka = convert_paisa_to_taka(system_balance)
     return schemas.SystemBalance(balance=system_balance_in_taka)
