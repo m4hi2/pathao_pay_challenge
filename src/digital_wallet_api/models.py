bf4e179b8eaa5399ae979a898de8ad68d4b9a3ad
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-10-30 07:25:29 +0600

    delete: old schema and model codes

--- src/digital_wallet_api/models.py
+++ src/digital_wallet_api/models.py
@@ -3,23 +3,3 @@ from datetime import datetime
 from sqlalchemy import Column, DateTime, Integer, String, Float
 
 from .database import Base
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), index=True)
-    hashed_pin = Column(String)
-    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
-    balance = Column(Float, default=5000)
-
-
-class Transaction(Base):
-    __tablename__ = "transactions"
-
-    id = Column(Integer, primary_key=True, index=True)
-    transaction_date = Column(DateTime(timezone=True), default=datetime.utcnow)
-    from_user_id = Column(Integer, index=True)
-    to_user_id = Column(Integer, index=True)
-    ammount = Column(Float)
--- src/digital_wallet_api/routers/users.py
+++ src/digital_wallet_api/routers/users.py
@@ -5,25 +5,3 @@ from digital_wallet_api.database import get_db
 
 
 router = APIRouter(tags=["Users"], prefix="/users")
-
-
-@router.post("/", response_model=schemas.User)
-def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    """Creates a new user in the system with initial balance of 5000 taka."""
-    db_user = repository.user.create_user(db, user=user)
-    if not db_user:
-        raise HTTPException(
-            status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Pin must be of 5 digits"
-        )
-    return db_user
-
-
-@router.get("/{id}", response_model=schemas.User)
-def get_user(id: int, db: Session = Depends(get_db)):
-    """Gets the user based on the id provided"""
-    user = repository.user.get_user_by_id(db, id)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User Not found"
-        )
-    return user
