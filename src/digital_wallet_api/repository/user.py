8488907d0c88d23f6b0281f012383877dc1016f9
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-10-30 08:32:25 +0600

    feat: user signup

--- src/digital_wallet_api/repository/user.py
+++ src/digital_wallet_api/repository/user.py
@@ -1,3 +1,23 @@
 from sqlalchemy.orm import Session
 
-from .. import models, schemas
+from digital_wallet_api import models, schemas
+
+
+def get_user_by_id(db: Session, id: int):
+    return db.query(models.User).filter(models.User.id == id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    if get_user_by_email(db, user.email):
+        return None
+
+    hased_pin = user.pin
+    db_user = models.User(email=user.email, name=user.name, pin=hased_pin)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
--- src/digital_wallet_api/routers/users.py
+++ src/digital_wallet_api/routers/users.py
@@ -5,3 +5,13 @@ from digital_wallet_api.database import get_db
 
 
 router = APIRouter(tags=["Users"], prefix="/users")
+
+
+@router.post("/", response_model=schemas.User)
+def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = repository.user.create_user(db=db, user=user)
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registerd."
+        )
+    return db_user
