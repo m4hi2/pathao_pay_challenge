58d8ae65e6ed7e2e7b34e1b3e13076571dfd0e4e
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-10-30 08:57:39 +0600

    feat: 5 digit pin requirement verification

--- src/digital_wallet_api/views/user.py
+++ src/digital_wallet_api/views/user.py
@@ -4,9 +4,34 @@ from digital_wallet_api import schemas, repository
 
 
 def signup(user: schemas.UserCreate, db: Session):
+    if not verify_pin_requirements(user.pin):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Pin has to be 5 digits"
+        )
     db_user = repository.user.create_user(db=db, user=user)
     if not db_user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registerd."
         )
     return db_user
+
+
+def verify_pin_requirements(pin: str) -> bool:
+    """According to the requirements, the pin has to be 5 digits.
+
+    Args:
+        pin (str): User provided pin
+
+    Returns:
+        bool: Wheather the pin meets requirements or not.
+    """
+
+    if len(pin) != 5:
+        return False
+
+    try:
+        int(pin)
+    except ValueError:
+        return False
+
+    return True
